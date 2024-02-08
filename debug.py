dc711404fdfa29140628c1373c1991f957356657
moses <moses.kingi@moringaschool.com>
2024-02-08 14:45:35 +0300

    review class added and debug imports for restrurant,customer and review added

--- debug.py
+++ debug.py
@@ -2,12 +2,13 @@
 import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import ipdb;
-
+from models import Base, Restaurant, Customer, Review
+import ipdb
 
 if __name__ == '__main__':
-    
     engine = create_engine('sqlite:///restaurants.db')
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
